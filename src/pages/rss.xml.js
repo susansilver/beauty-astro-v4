de12de701c2ecb94de279d6f33e9c6f8632e35a3
Susan Silver <hello@susansilver.net>
2023-12-07 17:33:19 -0800

    update rss feed xml

--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -1,15 +1,15 @@
+import { SITE_TITLE, SITE_DESCRIPTION } from '../consts';
 import rss from '@astrojs/rss';
 import { getCollection } from 'astro:content';
-import { SITE_TITLE, SITE_DESCRIPTION } from '../consts';
 
-export async function get(context) {
-	const posts = await getCollection('blog');
+export async function GET(context) {
+  const blog = await getCollection('blog');
 	return rss({
 		stylesheet: '/rss/styles.xsl',
 		title: SITE_TITLE,
 		description: SITE_DESCRIPTION,
 		site: context.site,
-		items: posts.map((post) => ({
+		items: blog.map((post) => ({
 			title: post.data.title,
 			pubDate: post.data.pubDate,
 			description: post.data.description,
